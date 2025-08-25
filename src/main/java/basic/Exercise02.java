2acddc4667621838daec682323ce40429bc93915
tientran <kimtientran0410@gmail.com>
2025-08-25 15:19:49 +0700

    Feature basic exercise02

--- src/main/java/basic/Exercise02.java
+++ src/main/java/basic/Exercise02.java
@@ -2,6 +2,12 @@ package basic;
 
 public class Exercise02 {
     public int sumDigits(int number) {
-        return number > 0 ? number % 10 + sumDigits(number / 10) : 0;
+        int sum = 0;
+        while (number > 0){
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+        //return number > 0 ? number % 10 + sumDigits(number / 10) : 0;
     }
 }
