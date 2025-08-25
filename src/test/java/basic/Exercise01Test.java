5878e3613a05e39f6d80c4768f299d710d45e692
tientran <kimtientran0410@gmail.com>
2025-08-25 14:55:37 +0700

    Feature basic exercise01

---
+++ src/test/java/basic/Exercise01Test.java
@@ -0,0 +1,18 @@
+package basic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Exercise01Test {
+    @Test
+    void shouldSumDigits_OK() {
+        final Exercise01 exercise01 = new Exercise01();
+        assertEquals(2, exercise01.GCD(2,4));
+        assertEquals(1, exercise01.GCD(2,3));
+        assertEquals(5, exercise01.GCD(0,5));
+        assertEquals(6, exercise01.LCM(2,3));
+        assertEquals(12, exercise01.LCM(3,4));
+
+    }
+}
